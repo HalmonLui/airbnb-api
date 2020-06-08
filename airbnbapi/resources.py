62d7771b239f7a7cc93d2bb3664c4ecd826c7863
Lui <luih@wit.edu>
2020-06-08 00:18:30 -0400

    remove unnecessary comments, add getListings endpoint

--- airbnbapi/resources.py
+++ airbnbapi/resources.py
@@ -1,33 +1,6 @@
 from airbnbapi import api
 from flask_restful import Resource, reqparse
 from flask import jsonify
-# @app.route('/')
-# def index():
-#     return 'Hello World!'
-
-# @app.route('/getListings')
-# parser = reqparse.RequestParser()
-# parser.add_argument('city', required=True)
-# parser.add_argument('state', required=True)
-# parser.add_argument('checkin')
-# parser.add_argument('checkout')
-# parser.add_argument('adults')
-# parser.add_argument('page')
-# parser.add_argument('search_type')
-# args = parser.parse_args(strict=True)
-
-# api.add_resource(UserAPI, '/users/<int:id>', endpoint = 'user')
-# class ListingsAPI(Resource):
-#     def get(self, id):
-#         pass
-#
-#     def put(self, id):
-#         pass
-#
-#     def delete(self, id):
-#         pass
-
-
 
 class Hello(Resource):
     def get(self):
@@ -41,14 +14,29 @@ class Hello(Resource):
         return jsonify({'data': data}), 201
 
 
-# another resource to calculate the square of a number
-class Square(Resource):
+# # another resource to calculate the square of a number
+# class Square(Resource):
+#
+#     def get(self, num):
+#
+#         return jsonify({'square': num**2})
 
-    def get(self, num):
-
-        return jsonify({'square': num**2})
+# Get Airbnb Listings
+class ListingsAPI(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('city', required=True)
+        parser.add_argument('state', required=True)
+        parser.add_argument('checkin')
+        parser.add_argument('checkout')
+        parser.add_argument('adults')
+        parser.add_argument('page')
+        parser.add_argument('search_type')
+        args = parser.parse_args(strict=True)
+        return args
 
 
 # adding the defined resources along with their corresponding urls
 api.add_resource(Hello, '/')
-api.add_resource(Square, '/square/<int:num>')
+# api.add_resource(Square, '/square/<int:num>')
+api.add_resource(ListingsAPI, '/getListings')
